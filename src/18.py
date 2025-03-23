abb9342617ed5817a4b1201e66bf34251e564345
vilmakouvala943 <vilmakouvala943@gmail.com>
2025-03-23 03:42:42 +0000

    Add 18.py

---
+++ src/18.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def calculate_area(length, width):
+    """
+    Calculate the area of a rectangle.
+    
+    Args:
+    length (float): The length of the rectangle.
+    width (float): The width of the rectangle.
+
+    Returns:
+    float: The area of the rectangle.
+    """
+    return length * width
