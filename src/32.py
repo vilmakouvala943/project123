2ee57e75190f8e5b772caa53d867c95975fab49f
vilmakouvala943 <vilmakouvala943@gmail.com>
2025-04-24 15:51:06 +0000

    Add 32.py

---
+++ src/32.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be positive integer"
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+
+    return fib_sequence
